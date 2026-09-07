d30bdd55e3a4e2396ae84490a51485d75f2ca488
Ara Hacopian <ara@hacopian.de>
2026-09-07 17:15:11 +0200

    Add 2015.1.1 solution

--- Aoc/Y2015/Day01.lean
+++ Aoc/Y2015/Day01.lean
@@ -2,8 +2,11 @@ import Aoc.Core
 
 namespace Aoc.Y2015.Day01
 
+def f (x : Char) : Int :=
+  if x == '(' then 1 else -1
+
 def part1 (input : String) : String :=
-  s!"unsolved: {(lines input).length} lines of input"
+  s!"{((input.toList.map f).sum)}"
 
 def part2 (input : String) : String :=
   s!"unsolved: {(lines input).length} lines of input"

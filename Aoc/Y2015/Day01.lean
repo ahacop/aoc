359771cd5e672825e965698604e74188561a0340
Ara Hacopian <ara@hacopian.de>
2026-09-08 14:25:49 +0200

    Simplify

--- Aoc/Y2015/Day01.lean
+++ Aoc/Y2015/Day01.lean
@@ -5,9 +5,6 @@ namespace Aoc.Y2015.Day01
 def f (x : Char) : Int :=
   if x == '(' then 1 else -1
 
-def g (h : (Char -> Int)) (c : Char) (x : Int) : Int :=
-  h c + x
-
 def part1 (input : String) : String :=
   s!"{((input.toList.map f).sum)}"
 
@@ -15,7 +12,7 @@ def findBasement
   (xs : Array Char) : Int := Id.run do
   let mut currentFloor := 0
   for h : i in [0:xs.size] do
-    currentFloor := g f xs[i] currentFloor
+    currentFloor := currentFloor + f xs[i]
     if currentFloor = -1 then return i + 1
   return 0
 

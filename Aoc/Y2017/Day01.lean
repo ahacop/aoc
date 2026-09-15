eadfae58043c8305376bc69e0ffa77d22df2944e
Ara Hacopian <ara@hacopian.de>
2026-09-15 16:18:51 +0200

    Refactor 2017 Day 1

--- Aoc/Y2017/Day01.lean
+++ Aoc/Y2017/Day01.lean
@@ -2,36 +2,37 @@ import Aoc.Core
 
 namespace Aoc.Y2017.Day01
 
-def parseStep (x : Char) : Except String Nat := do
-  let some n := x.toString.toNat?
-    | throw s!"bad {x.quote}"
-  pure n
+def parseStep (x : Char) : Except String Nat :=
+  match x.toString.toNat? with
+    | some n => pure n
+    | none => throw s!"{x.quote} is not a Nat"
 
 def parse (input : String) : Except String (List Nat) :=
   input.trimAscii.toString.toList.mapM parseStep
 
+def sumMatches (xs : List Nat) (offset : Nat) : Nat :=
+  xs.zip (xs.rotateLeft offset)
+    |>.filter (fun (x, y) => x = y)
+    |>.map (·.1)
+    |>.sum
+
 def part1 (input : String) : Except String String := do
   let xs <- parse input
-  let ys := xs.zip (xs.rotateLeft 1)
-    |>.filterMap fun x => if x.fst = x.snd then some (x.fst) else none
-  pure s!"{ys.sum}"
+  pure s!"{sumMatches xs 1}"
 
 def part2 (input : String) : Except String String := do
   let xs <- parse input
-  let skip := xs.toArray.size / 2
-  let ys := xs.zip (xs.rotateLeft skip)
-    |>.filterMap fun x => if x.fst = x.snd then some (x.fst) else none
-  pure s!"{ys.sum}"
+  pure s!"{sumMatches xs (xs.length / 2)}"
 
-#guard (part1 "1122") matches (.ok "3")
-#guard (part1 "1111") matches (.ok "4")
-#guard (part1 "1234") matches (.ok "0")
-#guard (part1 "91212129") matches (.ok "9")
+#guard part1 "1122" matches .ok "3"
+#guard part1 "1111" matches .ok "4"
+#guard part1 "1234" matches .ok "0"
+#guard part1 "91212129" matches .ok "9"
 
-#guard (part2 "1212") matches (.ok "6")
-#guard (part2 "1221") matches (.ok "0")
-#guard (part2 "123425") matches (.ok "4")
-#guard (part2 "123123") matches (.ok "12")
-#guard (part2 "12131415") matches (.ok "4")
+#guard part2 "1212" matches .ok "6"
+#guard part2 "1221" matches .ok "0"
+#guard part2 "123425" matches .ok "4"
+#guard part2 "123123" matches .ok "12"
+#guard part2 "12131415" matches .ok "4"
 
 end Aoc.Y2017.Day01
